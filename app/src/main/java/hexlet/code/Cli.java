faa97b19dfeafc1bd557df2af3475c3c88655241
racbracb1 <racbracb1@icloud.com>
2022-07-14 13:15:07 +0300

    add checkstyle

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,10 +3,10 @@ package hexlet.code;
 import java.util.Scanner;
 
 class Cli {
-        public static String greeting() {
-            Scanner userName = new Scanner(System.in);
-            String name;
-            name = userName.nextLine();
-            return "Hello, " + name + "!";
-        }
+    public static String greeting() {
+        Scanner userName = new Scanner(System.in);
+        String name;
+        name = userName.nextLine();
+        return "Hello, " + name + "!";
+    }
 }
