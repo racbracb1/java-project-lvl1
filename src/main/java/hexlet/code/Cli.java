508e646067489ab81691ee42939ec747ab61267f
racbracb1 <racbracb1@icloud.com>
2022-07-26 11:05:00 +0300

    fix Cli

--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -4,11 +4,6 @@ import java.util.Scanner;
 
 class Cli {
     public static String greeting() {
-        Scanner nameUser = new Scanner(System.in);
-        System.out.println("Welcome to the Brain Games!");
-        System.out.println("May I have your name?");
-        String name = nameUser.nextLine();
-        System.out.println("Hello, " + name + "!");
-        return name;
+        return Engine.gameGreeting();
     }
 }
