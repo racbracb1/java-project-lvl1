289ea865f268f05d83961b6186896bc190076589
racbracb1 <racbracb1@icloud.com>
2022-07-27 08:44:53 +0300

    fix Prime

--- src/main/java/hexlet/code/Games/Prime.java
+++ src/main/java/hexlet/code/Games/Prime.java
@@ -8,18 +8,19 @@ public class Prime {
     public static String prime() {
         var name = Engine.gameGreeting();
         final int countToWin = 3;
+        var randomNumber = Engine.randomNumber();
         int indexRightAnswers = 0;
         System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
         while (indexRightAnswers < countToWin) {
             boolean isSimple = true;
             //var randomNumber = Engine.randomNumber();
             Scanner answer = new Scanner(System.in);
-            System.out.println("Question: " + Engine.randomNumber() + "\nYour answer: ");
+            System.out.println("Question: " + randomNumber + "\nYour answer: ");
             //System.out.println("Your answer: ");
             String ans = answer.nextLine();
             int i = 2;
-            while (i <= Engine.randomNumber() / 2) {
-                if (Engine.randomNumber() % i == 0) {
+            while (i <= randomNumber / 2) {
+                if (randomNumber % i == 0) {
                     isSimple = false;
                     break;
                 }
