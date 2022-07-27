3951ad643af2b88e7b0a0b043931faed41f203fd
racbracb1 <racbracb1@icloud.com>
2022-07-27 08:40:24 +0300

    fix GCD

--- src/main/java/hexlet/code/Games/GCD.java
+++ src/main/java/hexlet/code/Games/GCD.java
@@ -17,7 +17,7 @@ public class GCD {
             var firstNumberString = Integer.toString(firstNumber);
             var secondNumberString = Integer.toString(secondNumber);
             int gcd = 1;
-            for (int i = 1; i <= Engine.randomNumber() && i <= Engine.randomNumber(); i++) {
+            for (int i = 1; i <= firstNumber && i <= secondNumber; i++) {
                 if (firstNumber % i == 0 && secondNumber % i == 0) {
                     gcd = i;
                 }
